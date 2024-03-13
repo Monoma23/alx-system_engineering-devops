4fa7d5b0d63b3130f3dc2765e721c2ed489812ed
Monoma23 <wydadi2372@gmail.com>
2024-03-12 17:41:19 -0700

    god

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,13 @@
+# enablinggg user login and opening files with no errorss
+
+# increasingg hard file limit for alxafrica user
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# increasing soft file limit for alxafrica user
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
